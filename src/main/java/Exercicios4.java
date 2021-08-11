a78acb93d7a567a24e3cbefde300dfed1a6a5d42
Douglas Dourado <61556868+DouglasNova@users.noreply.github.com>
2021-08-10 21:28:52 -0300

    Update Exercicios4.java

--- src/main/java/Exercicios4.java
+++ src/main/java/Exercicios4.java
@@ -9,7 +9,7 @@ public class Exercicios4 {
         int soma = 0;
         if (inicio >= n.length)
             return 0;
-        if (n[inicio] % 2 == 1){
+        if (n[inicio] % 2 != 0){
             soma ++;
         }
         return soma + getContaVetorImpares(n, inicio + 1);
